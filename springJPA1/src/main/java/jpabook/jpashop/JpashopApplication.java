7f92892b6858a376725b80649790e960d1fe648e
SYH <younghoon211@naver.com>
2024-06-09 17:51:15 +0900

    springJPA1 start

---
+++ springJPA1/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+	// 메인 메소
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}

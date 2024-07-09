c3028238c67e64df0fbca3bdee405628b3fcde9d
SYH <younghoon211@naver.com>
2024-07-09 12:27:06 +0900

    test: 테스트 페이지 생성

--- springJPA1/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ springJPA1/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-	// 메인 메소
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
--- springJPA1/src/main/java/jpabook/jpashop/test.java
+++
@@ -1,5 +0,0 @@
-package jpabook.jpashop;
-
-public class test {
-
-}x
